8b2607ba5afdd47f0186dc3c31d35f9af3e3b30a
Takayama Fumihiko <tekezo@pqrs.org>
2026-04-26 13:08:30 +0900

    Update tests

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -39,5 +39,42 @@ int main(void) {
     dispatcher = nullptr;
   };
 
+  "iokit_power_management::monitor can be destroyed immediately after async_start"_test = [] {
+    auto time_source = std::make_shared<pqrs::dispatcher::hardware_time_source>();
+    auto dispatcher = std::make_shared<pqrs::dispatcher::dispatcher>(time_source);
+    auto run_loop_thread = std::make_shared<pqrs::cf::run_loop_thread>();
+
+    auto monitor = std::make_unique<pqrs::osx::iokit_power_management::monitor>(dispatcher,
+                                                                                run_loop_thread);
+
+    monitor->async_start();
+    monitor = nullptr;
+
+    run_loop_thread->terminate();
+    run_loop_thread = nullptr;
+
+    dispatcher->terminate();
+    dispatcher = nullptr;
+  };
+
+  "iokit_power_management::monitor accepts async_start and async_stop back to back"_test = [] {
+    auto time_source = std::make_shared<pqrs::dispatcher::hardware_time_source>();
+    auto dispatcher = std::make_shared<pqrs::dispatcher::dispatcher>(time_source);
+    auto run_loop_thread = std::make_shared<pqrs::cf::run_loop_thread>();
+
+    auto monitor = std::make_unique<pqrs::osx::iokit_power_management::monitor>(dispatcher,
+                                                                                run_loop_thread);
+
+    monitor->async_start();
+    monitor->async_stop();
+    monitor = nullptr;
+
+    run_loop_thread->terminate();
+    run_loop_thread = nullptr;
+
+    dispatcher->terminate();
+    dispatcher = nullptr;
+  };
+
   return 0;
 }
