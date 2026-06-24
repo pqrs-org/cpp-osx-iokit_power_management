1e8f16d1d67db33c761fd1f9bf04a5227da49c9a
Takayama Fumihiko <tekezo@pqrs.org>
2026-06-25 08:58:27 +0900

    Add pending_power_response

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,3 +1,4 @@
+#include <atomic>
 #include <boost/ut.hpp>
 #include <pqrs/osx/iokit_power_management.hpp>
 
@@ -14,6 +15,105 @@ auto make_components() {
 int main() {
   using namespace boost::ut;
   using namespace boost::ut::literals;
+  using namespace std::chrono_literals;
+
+  "pending_power_response allows exactly once before callback starts"_test = [] {
+    int allow_count = 0;
+
+    pqrs::osx::iokit_power_management::detail::pending_power_response response(
+        123,
+        456,
+        [&](auto kernel_port, auto notification_id) {
+          ++allow_count;
+          expect(kernel_port == 123);
+          expect(notification_id == 456);
+        });
+
+    response.allow_power_change_if_callback_not_started();
+
+    expect(allow_count == 1_i);
+    expect(!response.get_wait()->wait_notice_for(0ms));
+    response.notify_wait();
+    expect(response.get_wait()->wait_notice_for(0ms));
+    expect(!response.try_start_callback());
+
+    response.allow_power_change_if_callback_not_started();
+    expect(allow_count == 1_i);
+  };
+
+  "pending_power_response does not allow after callback starts"_test = [] {
+    int allow_count = 0;
+
+    pqrs::osx::iokit_power_management::detail::pending_power_response response(
+        123,
+        456,
+        [&](auto, auto) {
+          ++allow_count;
+        });
+
+    expect(!response.get_wait()->wait_notice_for(1ms));
+    expect(response.try_start_callback());
+
+    response.allow_power_change_if_callback_not_started();
+
+    expect(allow_count == 0_i);
+    expect(!response.get_wait()->wait_notice_for(0ms));
+    response.notify_wait();
+    expect(response.get_wait()->wait_notice_for(0ms));
+  };
+
+  "pending_power_response allows when queued dispatcher callback is detached before running"_test = [] {
+    auto time_source = std::make_shared<pqrs::dispatcher::hardware_time_source>();
+    auto dispatcher = std::make_shared<pqrs::dispatcher::dispatcher>(time_source);
+
+    pqrs::dispatcher::extra::dispatcher_client blocker(dispatcher);
+    pqrs::dispatcher::extra::dispatcher_client target(dispatcher);
+
+    auto blocker_started = pqrs::make_thread_wait();
+    auto release_blocker = pqrs::make_thread_wait();
+
+    expect(blocker.enqueue_to_dispatcher([blocker_started, release_blocker] {
+      blocker_started->notify();
+      release_blocker->wait_notice();
+    }));
+
+    blocker_started->wait_notice();
+
+    std::atomic<int> allow_count = 0;
+    std::atomic<bool> callback_ran = false;
+
+    auto response = std::make_shared<pqrs::osx::iokit_power_management::detail::pending_power_response>(
+        123,
+        456,
+        [&](auto kernel_port, auto notification_id) {
+          ++allow_count;
+          expect(kernel_port == 123);
+          expect(notification_id == 456);
+        });
+    auto wait = response->get_wait();
+
+    expect(target.enqueue_to_dispatcher([response, wait, &callback_ran] {
+      callback_ran = true;
+
+      if (response->try_start_callback()) {
+        wait->notify();
+      }
+    }));
+
+    target.detach_from_dispatcher();
+    response->allow_power_change_if_callback_not_started();
+    response->notify_wait();
+
+    release_blocker->notify();
+    blocker.detach_from_dispatcher();
+
+    expect(allow_count.load() == 1_i);
+    expect(!callback_ran.load());
+    expect(wait->wait_notice_for(0ms));
+
+    dispatcher->terminate();
+    dispatcher = nullptr;
+  };
 
   "iokit_power_management::monitor"_test = [] {
     auto [time_source, dispatcher, run_loop_thread] = make_components();
